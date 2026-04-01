45d1c235bc30f1da16996f0cf459f86c0dbc6d99
Jasper <jasper.scheel2001@gmail.com>
2026-04-01 17:51:38 +0200

    refactored and tested code

--- experiment.py
+++ experiment.py
@@ -12,7 +12,7 @@ import pandas as pd
 from itertools import count
 
 from Q_net import DQN
-from utils import ExperienceReplay, optimize_model
+from utils import ExperienceReplay, optimize_model, trans
 device = torch.device(
     "cuda" if torch.cuda.is_available() else
     "mps" if torch.backends.mps.is_available() else
@@ -48,16 +48,6 @@ def epsilon_greedy(model, state, epsilon):
     else:
         return torch.tensor([[env.action_space.sample()]], device=device, dtype=torch.long)
 
-def update_target_net(target_net, policy_net,tau,TN):
-    target_net_state_dict = target_net.state_dict()
-    policy_net_state_dict = policy_net.state_dict()
-    if TN:
-        for key in policy_net_state_dict:
-            target_net_state_dict[key] = policy_net_state_dict[key]*tau + target_net_state_dict[key]*(1-tau)
-    else:
-        target_net_state_dict = policy_net.state_dict()
-    target_net.load_state_dict(target_net_state_dict)    
-
 def evla_policy(policy_net, eval_iterations = 10):
     eval_env = gym.make("CartPole-v1")
     episode_returns = []
@@ -79,10 +69,67 @@ def evla_policy(policy_net, eval_iterations = 10):
     eval_env.close()
     return np.mean(episode_returns)
 
+def optimize_model(policy_net:DQN, target_net:DQN, memory:ExperienceReplay, batch_size:int, gamma:float, device:torch.device, optim):
+    if len(memory) < batch_size:
+        return policy_net, optim
+    
+    transitions = memory.sample(batch_size)
+    batch = trans(*zip(*transitions))
+
+    non_final_mask = torch.tensor(tuple(map(lambda s: s is not None, batch.s_next)), device=device, dtype=torch.bool)
+    non_final_next_states = torch.cat([s for s in batch.s_next if s is not None])
+
+    state_batch = torch.cat(batch.s)
+    action_batch = torch.cat(batch.a)
+    reward_batch = torch.cat(batch.r)
+
+    state_action_values = policy_net(state_batch).gather(1, action_batch)
+
+    next_state_values = torch.zeros(batch_size, device=device)
+    with torch.no_grad():
+        next_state_values[non_final_mask] = target_net(non_final_next_states).max(1).values
+
+    expected_state_action_values = (next_state_values * gamma) + reward_batch
+
+    criterion = nn.MSELoss()
+    loss = criterion(state_action_values, expected_state_action_values.unsqueeze(1))
+
+    optim.zero_grad()
+    loss.backward()
+    torch.nn.utils.clip_grad_value_(policy_net.parameters(), 100)
+    optim.step()
+    return policy_net, optim
+
+def optimize_model_no_ER(policy_net:DQN, target_net:DQN, memory:ExperienceReplay, gamma:float, device:torch.device, optim):
+    transitions = memory.sample(1)
+    batch = trans(*zip(*transitions))
+    s = torch.cat(batch.s)
+    a = torch.cat(batch.a)
+    r = torch.cat(batch.r)
+
+    if type(batch.s_next) != torch.tensor:
+        expected_state_action_value = r
+    else:
+        sp = torch.cat(batch.s_next)
+        with torch.no_grad():
+            next_state_values = target_net(sp).max(1).values
+        expected_state_action_value = (next_state_values * gamma) + r
+
+    state_action_value = policy_net(s).gather(1, a)
+    criterion = nn.MSELoss()
+    loss = criterion(state_action_value, expected_state_action_value.unsqueeze(1))
+
+    optim.zero_grad()
+    loss.backward()
+    torch.nn.utils.clip_grad_value_(policy_net.parameters(), 100)
+    optim.step()
+    return policy_net, optim
+
 def train_dqn(
         env,
         policy_net, 
         target_net,
+        update_data_ratio = 4,
         memory_capacity = 10000,  
         budget = 1e5,
         gamma = 0.99,
@@ -98,8 +145,7 @@ def train_dqn(
     eval_timesteps = []
     eval_returns = []
     counter = 0
-    if not ER:
-        batch_size = 1
+    if not TN: update_data_ratio = 1
     while counter < budget:
         state, _ = env.reset()
         state = torch.tensor(state, dtype=torch.float32, device=device).unsqueeze(0)
@@ -123,21 +169,28 @@ def train_dqn(
 
             state = next_state
 
-            policy_net, optimizer = optimize_model(
-                policy_net=policy_net,
-                target_net=target_net,
-                memory=memory,
-                batch_size=batch_size,
-                gamma=gamma,
-                device=device,
-                optim=optimizer
-            )
-            update_target_net(
-                target_net=target_net,
-                policy_net=policy_net,
-                tau=tau,
-                TN=TN
-            )
+            if not ER:
+                policy_net,optimizer = optimize_model_no_ER(
+                    policy_net=policy_net,
+                    target_net=target_net,
+                    memory=memory,
+                    gamma=gamma,
+                    device=device,
+                    optim=optimizer
+                )
+            else:
+                policy_net, optimizer = optimize_model(
+                    policy_net=policy_net,
+                    target_net=target_net,
+                    memory=memory,
+                    batch_size=batch_size,
+                    gamma=gamma,
+                    device=device,
+                    optim=optimizer
+                )
+            if counter % update_data_ratio == 0:
+                target_net.load_state_dict(policy_net.state_dict())
+
             
             counter += 1
 
@@ -151,7 +204,13 @@ def train_dqn(
         
 
 
-eval_timesteps, eval_returns = train_dqn(env=env,policy_net=policy_net,target_net=target_net)
+eval_timesteps, eval_returns = train_dqn(
+    env=env,
+    policy_net=policy_net,
+    target_net=target_net,
+    TN=True,
+    ER=False,
+)
 df = pd.DataFrame({"eval_timesteps": eval_timesteps, "eval_returns":eval_returns})
 df.to_csv('Data.csv')
 print('done')
