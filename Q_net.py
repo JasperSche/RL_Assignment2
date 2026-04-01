cd2c5bce17abb02984483fedeb1b2c6e03961f04
jasper <jasper.scheel2001@gmail.com>
2026-04-02 00:23:09 +0200

    added variable network size

--- Q_net.py
+++ Q_net.py
@@ -3,15 +3,16 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class DQN(nn.Module):
-
-    def __init__(self, n_observations, n_actions):
+    def __init__(self, n_observations, n_actions, n_layers = 1):
         super(DQN, self).__init__()
+        self.n_layers = n_layers
         self.input = nn.Linear(n_observations, 128)
-        self.h1 = nn.Linear(128, 128)
+        for idx in range(self.n_layers):
+            setattr(self, 'layer%d' % (idx+1), nn.Linear(128,128))
         self.out = nn.Linear(128, n_actions)
 
     def forward(self, observations):
         x = F.relu(self.input(observations))
-        x = F.relu(self.h1(x))
+        for idx in range(self.n_layers):
+            x = F.relu(getattr(self, 'layer%d' % (idx+1))(x))
         return self.out(x)
-    
