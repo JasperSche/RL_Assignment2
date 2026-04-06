d2f417d59451d2e1ef265c9c2d7b49643870d0a4
Jasper Scheel <jasper.scheel2001@gmail.com>
2026-04-06 17:16:46 +0200

    Renamed file and edited the plotting code a little

--- plot_configuraton_tests.py
+++ plot_configuraton_tests.py
@@ -3,6 +3,8 @@ import csv
 import os
 import numpy as np
 from scipy.signal import savgol_filter
+
+
 plot_titles = ['ER','TN&ER','Naive','TN']
 x_list = []
 y_list = []
@@ -32,25 +34,20 @@ while index < len(files):
         std_list.append(std)
     index +=1
 smoothing_window = 81
-#learning_curve = smooth(learning_curve,smoothing_window) # additional smoothing
 
 fig, ax = plt.subplots()
 ax.set_title(f'Experiment: Results for Naive, ER, TN and ER&TN implementations.')
-ax1 = ax.twinx()
-leg = []
 for i in range(len(x_list)):
     smooth = savgol_filter(y_list[i],smoothing_window,2)
     err = std_list[i]/np.sqrt(5)
 
     err_smooth = savgol_filter(err,smoothing_window,2)
-    
+
     ax.set_xlabel('Timestep')
     ax.set_ylabel('Episode Returns')
-    lns1 = ax.plot(x_list[i], smooth, label = f'{plot_titles[i]}')
-    lns2 = ax.fill_between(x_list[i],smooth-err_smooth,smooth+err_smooth,alpha=0.2)
-    leg += lns1 
-
-labs = [l.get_label() for l in leg]    
-ax.legend(leg, labs, loc=0)    
+    ax.plot(x_list[i], smooth, label = f'{plot_titles[i]}')
+    ax.fill_between(x_list[i],smooth-err_smooth,smooth+err_smooth,alpha=0.2)
+ 
+ax.legend()    
 plt.savefig(f"Full_Run_Results/result_smoothing{smoothing_window}.png", dpi=300, bbox_inches='tight')
 plt.show()
